613e36bf1199b5d4ad207c29f5990702cd53de04
JayManjrekar0827 <jmanjrekar@ucsd.edu>
2026-04-24 20:26:56 -0700

    fixed this

--- projects/projects.js
+++ projects/projects.js
@@ -1,13 +1,17 @@
 import { fetchJSON, renderProjects } from "../global.js";
 
-const projects = await fetchJSON("../lib/projects.json");
-const projectsContainer = document.querySelector(".projects");
+async function initProjectsPage() {
+  const projects = await fetchJSON("../lib/projects.json");
+  const projectsContainer = document.querySelector(".projects");
 
-if (projectsContainer) {
-  renderProjects(projects, projectsContainer, "h2");
+  if (projectsContainer) {
+    renderProjects(projects, projectsContainer, "h2");
+  }
+
+  const titleElement = document.querySelector(".projects-title");
+  if (titleElement && Array.isArray(projects)) {
+    titleElement.textContent = `${projects.length} Projects`;
+  }
 }
 
-const titleElement = document.querySelector(".projects-title");
-if (titleElement && Array.isArray(projects)) {
-  titleElement.textContent = `${projects.length} Projects`;
-}
+initProjectsPage();
